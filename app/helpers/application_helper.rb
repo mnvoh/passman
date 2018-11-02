64057503db91107f83ec33bf74627307d63ac9d9
Milad Nozari <mnvoh90@gmail.com>
2018-11-02 11:37:29 +0330

    Finish show password page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_datetime(datetime)
+    if datetime.strftime('%Y') != DateTime.now.strftime('%Y')
+      datetime.strftime("%a %b %e, %Y, %H:%M %p (%z)")
+    else
+      datetime.strftime("%a %b %e, %H:%M %p (%z)")
+    end
+  end
 end
