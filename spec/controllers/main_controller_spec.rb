f75e5805d8095beeae329b23e6c94779cb461d26
Milad Nozari <mnvoh90@gmail.com>
2019-03-01 13:50:51 +0330

    Setup capybara

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe MainController, type: :controller do
-
+  context '#index' do
+    it 'should show the home page for guests' do
+      visit 
+    end
+  end
 end
