6920ba88b9d8057aece5d86fcb63e2d8888652fd
Milad Nozari <mnvoh90@gmail.com>
2019-03-01 17:10:12 +0330

    Add test for homepage

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe MainController, type: :controller do
   context '#index' do
     it 'should show the home page for guests' do
-      visit 
+      get :index 
+      expect(response.status).to eq(200)
     end
   end
 end
