7d075b5f07c61ea6aa21b7bfd6ed6416b3fb8c19
Milad <mnvoh90@gmail.com>
2019-03-08 12:30:51 +0330

    Add faker and use it in the user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,9 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    name { 'Users Name' }
-    email { 'users_name@example.com' }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
     password { '123123' }
     created_at { 10.days.from_now }
     updated_at { DateTime.now }
