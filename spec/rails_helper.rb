e1acb0a62d792a3213f6b89f02db6b7fbc03ec84
Milad <mnvoh90@gmail.com>
2019-03-15 09:05:31 +0330

    Implement the specs for the passwords controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+TEST_PASSWORD = 'testpassword143'
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
