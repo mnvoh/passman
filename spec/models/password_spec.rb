a6af2303b7917580294ad9248f5ce67487ecba81
Milad <mnvoh90@gmail.com>
2019-03-15 14:02:35 +0330

    Implement password model specs

--- spec/models/password_spec.rb
+++ spec/models/password_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Password, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:password) { create(:password) }
+  it 'user should exist' do
+    expect(password.user).not_to be(nil)
+  end
+
+  it 'unencrypted data shouldn\'t be save' do
+    password.password = '123123'
+    expect { password.save }.to raise_error(/encrypt_password/)
+  end
+
+  it 'encryption should work as expected' do
+    password.iv = 'oW3QpoZOTg+6aD8BgbBf7Q=='
+    password.salt = '6Py5zWaPCBXUMHK0CxKrdfJdH/SHB4JcPeQ3DOt15EQ='
+    password.password = TEST_PASSWORD
+    password.description = TEST_PASSWORD
+    password.encrypt_data(TEST_PASSWORD)
+    expect("#{password.password}#{password.description}").to eq(':aes256b64:P1Y80mE53YJ6mQ91EV8mbA==:aes256b64:P1Y80mE53YJ6mQ91EV8mbA==')
+  end
 end
